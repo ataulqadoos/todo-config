b4e0b5c638d1214cc944d8bac0018ae313259ccc
ataulqadoos <ataulqadoos@gmail.com>
2020-02-12 10:49:06 -0500

    Initial commit

---
+++ src/main/java/com/aqadoos/todo/config/TodoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.aqadoos.todo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aqadoos/todo/config/TodoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqadoos.todo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
